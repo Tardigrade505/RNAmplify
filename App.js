18388b7795f5f5da01bdb8e7fac52e912d3b6d7e
Elliot Radsliff <elliot@freemarkethealth.com>
2021-01-17 14:05:18 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Amplify, { Auth, API } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
+Amplify.configure({
+    Auth: {
+        region: 'us-west-2',
+        userPoolId: 'us-west-2_SR0KH9zON',
+        userPoolWebClientId: '5ki7bnuimr2ujomrdi6kqk5u9s',
+        mandatorySignIn: false,
+    },
+    API: {
+      endpoints: [
+        {
+          name: "accounts",
+          endpoint:
+            "https://fvilydx9s4.execute-api.us-west-2.amazonaws.com/prod/",
+        },
+      ],
+    },
+    Analytics: {
+      disabled: true,  // Disabling for now to remove warnings
+    },
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
+
+
+async function signOut() {
+  try {
+      await Auth.signOut();
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
+}
+
+const App = () => {
+  const [helloState, setHelloState] = useState('Initial');
+  const [accountState, setAccountState] = useState('Initial');
+  const [currentJWT, setCurrentJWT] = useState('');
+  const [currentUserInfo, setCurrentUserInfo] = useState({});
+
+  useEffect(() => {
+    async function setSessionInfo() {
+      setCurrentJWT((await Auth.currentSession()).getIdToken().getJwtToken());
+      setCurrentUserInfo(await Auth.currentUserInfo())
+    }
+    setSessionInfo();
+  }, [Auth])
+
+  async function fetchHello() {
+    const hello = await API.get('accounts', 'hello', { 
+      headers: { 
+        'Authorization': `Bearer ${currentJWT}`,
+      },
+    });
+    console.log(hello);
+    setHelloState(hello);
+  }
+
+  async function fetchAccount() {
+    console.log(`Current User Info: ${JSON.stringify(currentUserInfo)}`);
+    const account = await API.get('accounts', `user_account?handle=${currentUserInfo.username}`, { 
+      headers: { 
+        'Authorization': `Bearer ${currentJWT}`,
+      },
+    });
+    console.log(account);
+    setAccountState(account);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => fetchHello()}
+        title="Say Hello!"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Text>{helloState}</Text>
+      <Button
+        onPress={() => fetchAccount()}
+        title="View Account!"
+        color="#192f91"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Text>{JSON.stringify(accountState)}</Text>
+      <Button
+        onPress={() => signOut()}
+        title="Log Out"
+        color="#d16d15"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -19,3 +107,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App)
